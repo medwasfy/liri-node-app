72a2e0fd77dac73abd4dd270af959360e7ad2647
Med Wasfy <medwasfy@gmail.com>
2018-07-09 20:34:50 -0500

    i call this myth busted

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # liri-node-app
-LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+LIRI is a _Language_ Interpretation and Recognition Interface. 
+
+LIRI will be a command line node app that takes in parameters and gives you back data.
+
+# LIRI will do any of the below command when you enter them into the command line.
+
+1. my-tweets
+2. spotify-this-song
+3. movie-this
+4. do-what-it-says
+Type in node liri.js to get the instructions on how to enter the commands correctly. So if you were to type the below command you'd get the lastest tweets.
+
+>>>>>> spotify-this-song 
+shows the following information about the song in the terminal:
+
+. artist(s)
+. song name
+. preview link of the song from spotify
+. album that the song is a part of
+
+>>>>>> movie-this
+this would output the following information to the terminal:
+
+Title
+Year
+IMDB Rating
+Country
+Language
+Plot
+Actors
+Rotten Tomatoes Rating
+
+>>>>>> node liri.js do-what-it-says
+
+take the command from random.tx file and executes it with the appropriate function
+
+
+>>>>>> These are the npm packages I used and are needed to run the app
+
+fs package in node
+twitter
+spotify
+request
--- liri.js
+++ liri.js
@@ -8,33 +8,40 @@ var fs = require("fs"); //reads and writes files
 
 var Twitter = require("twitter"); 
 
-var Spotify = require ("node-spotify-api");
+var Spotify = require("node-spotify-api");
 
 var request = require("request");
 
 var keys = require("./keys.js");
 
-var http = require("http");
-
 var liriArg = process.argv[2];
 var userInput = process.argv[3];
+
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
 
 // ---------------------------------------------------------------------------------------------------------------
 // commands for liri
 // ========================================================================================================
-switch(liriArg) {
-    case "my-tweets": myTweets(); break;
-    case "spotify-this-song": spotifyThisSong(); break;
-    case "movie-this": movieThis(); break;
-    case "do-what-it-says": doWhatItSays(); break;
-    // Instructions displayed in terminal to the user
-    default: console.log("\r\n" +"Type one of the following commands after 'node liri.js' : " +"\r\n"+
-        "1. my-tweets 'any twitter name' " +"\r\n"+
-        "2. spotify-this-song 'any song name' "+"\r\n"+
-        "3. movie-this 'any movie name' "+"\r\n"+
-        "4. do-what-it-says."+"\r\n"+
-        "If more than one word, put all in quotation marks!");
-};
+function liriMain(command, arg){
+    switch(command) {
+        case "my-tweets": myTweets(); break;
+        case "spotify-this-song": spotifyThisSong(arg); break;
+        case "movie-this": movieThis(arg); break;
+        case "do-what-it-says": doWhatItSays(); break;
+        // Instructions displayed in terminal to the user
+        default: console.log("\r\n" +"Type one of the following commands after 'node liri.js' : " +"\r\n"+
+            "1. my-tweets 'any twitter name' " +"\r\n"+
+            "2. spotify-this-song 'any song name' "+"\r\n"+
+            "3. movie-this 'any movie name' "+"\r\n"+
+            "4. do-what-it-says."+"\r\n"+
+            "If more than one word, put all in quotation marks!");
+    }
+}
+
+
 // ---------------------------------------------------------------------------------------------------------------
 // Functions
 // ========================================================================================================
@@ -42,50 +49,50 @@ switch(liriArg) {
 // Tweet function, uses the Twitter module to call the Twitter api
 function myTweets() {
     var client = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        consumer_key: keys.twitter.consumer_key,
+        consumer_secret: keys.twitter.consumer_secret,
+        access_token_key: keys.twitter.access_token_key,
+        access_token_secret: keys.twitter.access_token_secret
       });
-    var twitterUsername = userInput;
-    if(!twitterUsername){
-        twitterUsername = "bootcampfiles";
-    }
-    params = {screen_name: twitterUsername};
+    
+    params = {screen_name: 'bootcampfiles'};
     
     // GET request for last 20 (default) tweets on my timeline
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if(error) { 
-            console.log('Error occurred: ' + error);
-        } else { 
-        console.log("My Recent Tweets");
-
-        for(var i = 0; i < tweets.length; i++) {
-            console.log("( #" + (i + 1) + " )  " + tweets[i].text);
-            console.log("Created:  " + tweets[i].created_at);
-            console.log("=======================================");
+        if (!error) {
+            for(var i = 0; i < tweets.length; i++) {
+                
+                var twitterResults = 
+                "@" + tweets[i].user.screen_name + ": " + 
+                tweets[i].text + "\r\n" + 
+                tweets[i].created_at + "\r\n" + 
+                "================================================================================" + "\r\n";
+                console.log(twitterResults);
+                log(twitterResults); 
+            }
+        }  else {
+            console.log("Error :"+ error);
+            return;
         }
-      }
+    //     if(error) { 
+    //         console.log('Error occurred: ' + error);
+    //     } else { 
+    //     console.log("My Recent Tweets");
+
+    //     for(var i = 0; i < tweets.length; i++) {
+    //         console.log("( #" + (i + 1) + " )  " + tweets[i].text);
+    //         console.log("Created:  " + tweets[i].created_at);
+    //         console.log("=======================================");
+    //     }
+    //   }
     });
     }
 
 
 // ========================================================================================================
 
-// Spotify function, uses the Spotify module to call the Spotify api
+// Spotify function, uses the Spotify module to call the Spotify app
 
-var spotify = new Spotify({
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
-  });
-//   function spotifyThisSong(userInput) {spotify.search({ type: 'track', query: userInput }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-   
-//   console.log(data); 
-//   });
-// };
 function spotifyThisSong(userInput) {
     if(!userInput){
         userInput = "who let the dogs out";
@@ -103,25 +110,27 @@ function spotifyThisSong(userInput) {
                     "Preview Url: " + songInfo[i].preview_url + "\r\n" +
                     "=====================================================" + "\r\n";
                     console.log(spotifyResults);
+                    log(spotifyResults);
                 }
             }
         }	else {
-            console.log("Error :"+ err);
+            console.log(data);
+            // console.log("Error :"+ err);
             return;
         }
     });
-};
+}
 
-// ==========================================================================================================
+// // ==========================================================================================================
 
-// Movie function, uses the Request module to call the OMDB api
-function movieThis(){
+// // Movie function, uses the Request module to call the OMDB api
+function movieThis(userInput){
     var movie = userInput;
     if(!movie){
         movie = "minions";
     }
     params = movie;
-    request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+    request("http://www.omdbapi.com/?apikey=a09ba9af&t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
         if (!error && response.statusCode == 200) {
 
             var movieObject = JSON.parse(body);
@@ -129,18 +138,20 @@ function movieThis(){
             "Title: " + movieObject.Title+"\r\n"+
             "Year: " + movieObject.Year+"\r\n"+
             "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+            "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
             "Country: " + movieObject.Country+"\r\n"+
             "Language: " + movieObject.Language+"\r\n"+
             "Plot: " + movieObject.Plot+"\r\n"+
             "Actors: " + movieObject.Actors+"\r\n";
 
             console.log(movieResults);
+            log(movieResults);
         } else {
             console.log("Error :"+ error);
             return;
         }
     });
-};
+}
 
 // ========================================================================================================
 
@@ -149,9 +160,25 @@ function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function(error, data){
         if (!error) {
             doWhatItSaysResults = data.split(",");
-            spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+            
+            liriMain(doWhatItSaysResults[0],doWhatItSaysResults[1]);
+            console.log(doWhatItSaysResults);
+            log(doWhatItSaysResults);
         } else {
             console.log("Error occurred" + error);
         }
     });
-};
+}
+
+// ========================================================================================================
+// activating log function
+
+function log(logResults) {
+    fs.appendFile("log.txt", logResults, (error) => {
+      if(error) {
+        throw error;
+      }
+    });
+  }
+
+liriMain(liriArg, userInput);
